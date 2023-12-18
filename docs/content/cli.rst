d3f5dcc968970f82cd156250d0a3039a2f5a2474
Gregory Poole <gbpoole@gmail.com>
2023-12-18 15:44:09 +1100

    Initial commit of template code

---
+++ docs/content/cli.rst
@@ -0,0 +1,8 @@
+short_project_name CLI Documentation
+====================================
+
+This package provides a CLI interface through which common tasks can be accomplished.
+
+.. click:: short_project_name.cli:cli
+   :prog: short_project_name
+   :show-nested:
---
+++ python/short_project_name/tests/test_cli.py
@@ -0,0 +1,44 @@
+import short_project_name
+import short_project_name.cli as cli
+from click.testing import CliRunner
+from pathlib import Path
+
+
+def test_cli_help(tmp_path: Path) -> None:
+    """Make sure that help runs without an error
+
+    Parameters
+    ----------
+    tmp_path : Path
+        Temporary path, generated from a pytest fixture
+    """
+    runner = CliRunner()
+    with runner.isolated_filesystem(temp_dir=tmp_path):
+        result = runner.invoke(
+            cli.cli,
+            [
+                "--help",
+            ],
+        )
+        assert result.exit_code == 0
+
+
+def test_cli_version(tmp_path: Path) -> None:
+    """Make sure that version runs without an error and generates the correct result
+
+    Parameters
+    ----------
+    tmp_path : Path
+        Temporary path, generated from a pytest fixture
+    """
+    runner = CliRunner()
+    with runner.isolated_filesystem(temp_dir=tmp_path):
+        result = runner.invoke(
+            cli.cli,
+            [
+                "--version",
+            ],
+        )
+        assert result.exit_code == 0
+
+        assert result.output == f"cli, version {short_project_name.__version__}\n"
